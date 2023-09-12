c7e860037a7790bf72bf6c4190d55bc9274307e1
Angel Benitez <benitezangelmatias@gmail.com>
2023-09-12 20:36:52 -0300

    Se creo un archovo pruebas.txt y se agrego un comentario en el archivo amb_cliente.php en la linea 9; Fecha:12/09/2023; Hora: 20:35

--- abm_cliente/abm_cliente.php
+++ abm_cliente/abm_cliente.php
@@ -6,6 +6,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>ABM_CLIENTE</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-4bw+/aepP/YC94hEpVNVgiZdgIC5+VKNBQNGCHeKRQN+PtmoHDEXuppvnDJzQIu9" crossorigin="anonymous">
+    <!--para probar el git status-->
     <style>
         .shadow-custom {
             box-shadow: rgba(0, 0, 0, 0.4) 0px 30px 90px;
